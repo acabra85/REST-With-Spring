3a65c76d527c79bd3a065653580367a74819c3ab
Agustin Cabra <cabra.agustin@gmail.com>
2021-08-12 18:52:31 +0200

    indenting the json response and fail on unknown properties

--- m1-lesson2-start/um-webapp/src/main/java/com/baeldung/um/spring/UmWebConfig.java
+++ m1-lesson2-start/um-webapp/src/main/java/com/baeldung/um/spring/UmWebConfig.java
@@ -1,15 +1,38 @@
 package com.baeldung.um.spring;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+import java.util.Optional;
 
 @Configuration
 @ComponentScan({ "com.baeldung.um.web" })
 @EnableWebMvc
-public class UmWebConfig {
+public class UmWebConfig extends WebMvcConfigurerAdapter {
 
-    public UmWebConfig() {
+    public UmWebConfig
+            () {
         super();
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Optional<HttpMessageConverter<?>> first = converters.stream()
+                .filter(c -> c instanceof AbstractJackson2HttpMessageConverter)
+                .findFirst();
+
+        if(first.isPresent()) {
+            final AbstractJackson2HttpMessageConverter converter = (AbstractJackson2HttpMessageConverter) first.get();
+            converter.getObjectMapper()
+                    .enable(SerializationFeature.INDENT_OUTPUT)
+                    .enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        }
     }
 }
